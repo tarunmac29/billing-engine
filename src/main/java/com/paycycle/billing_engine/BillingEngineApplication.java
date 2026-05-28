f0dea5385d504a5474f121dde4ee47a2b9ffb05c
tarunmac29 <tarunmachhi29@gmail.com>
2026-05-28 13:39:32 +0530

    feat : Initial Project scaffold from Spring Intilizer

---
+++ src/main/java/com/paycycle/billing_engine/BillingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.paycycle.billing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paycycle/billing_engine/BillingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paycycle.billing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
